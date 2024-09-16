9e1434582889148208267541e37538cb813a85fe
umarov-i <i.umarov@newuu.uz>
2024-09-16 14:41:44 +0500

    problem3

---
+++ problem3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+char s;
+int h;
+    if (s == 'M' and h < 1.70) {
+        cout << "Short" << endl;
+    }
+    else if (s == 'M' and h >= 1.70 and h < 1.85) {
+        cout << "Normal" << endl;
+    }
+    else if (s == 'M' and h >= 1.85) {
+        cout << "Tall" << endl;
+    }
+    else if (s == 'F' and h < 1.60) {
+        cout << "Short" << endl;
+    }
+    else if (s == 'F' and h >= 1.60 and h < 1.75) {
+        cout << "Normal" << endl;
+    }
+    else if (s == 'F' and h >= 1.75) {
+        cout << "Tall" << endl;
+    }
+    return 0;
+}
