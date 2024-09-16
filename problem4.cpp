f2094e07eeb1d6e62ec1f8fa1410ecc4c09a313b
umarov-i <i.umarov@newuu.uz>
2024-09-16 15:25:03 +0500

    problem5

--- problem4.cpp
+++ problem4.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
+#include <set>
 using namespace std;
 int main() {
 int n1, n2, n3, n4;
 cin >> n1 >> n2 >> n3 >> n4;
-    int cnt = 0;
-    if (n1 == n2) {
-        cnt += 1;
-    }
-    else if (n1 == n3) {
-        cnt += 1;
-    }
-    else if (n1 == n4) {
-        cnt += 1;
-    }
-    else if (n2 == n3) {
-        cnt += 1;
-    }
-    else if (n2 == n4) {
-        cnt += 1;
-    }
-    else if (n3 == n4) {
-        cnt += 1;
-    }
-    cout << cnt << endl;
+    cin >> n1 >> n2 >> n3 >> n4;
+    std::set<int> numbers1 = {n1, n2, n3, n4};
+
+    numbers1.size();
+    cout << 4 - numbers1.size() << endl;
     return 0;
 }
