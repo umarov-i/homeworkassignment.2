ca2dab8a2446bf2bd6bca2f219e4982000cf1fcf
umarov-i <i.umarov@newuu.uz>
2024-09-16 14:42:13 +0500

    problem4

---
+++ problem4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n1, n2, n3, n4, n5;
+    int cnt = 0;
+    if (n1 == n2) {
+        cnt += 1;
+    }
+    else if (n1 == n3) {
+        cnt += 1;
+    }
+    else if (n1 == n4) {
+        cnt += 1;
+    }
+    else if (n2 == n3) {
+        cnt += 1;
+    }
+    else if (n2 == n4) {
+        cnt += 1;
+    }
+    else if (n3 == n4) {
+        cnt += 1;
+    }
+    cout << cnt << endl;
+    return 0;
+}
