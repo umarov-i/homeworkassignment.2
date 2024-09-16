ceecda0bcf36f4dc960d968621a9c20b4dd0a5e7
umarov-i <i.umarov@newuu.uz>
2024-09-16 14:41:10 +0500

    problem2

---
+++ problem2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    if (a == 1) {
+        cout << "Bus" << endl;
+    }
+    else if (b == 1) {
+        cout << "Walk" << endl;
+    }
+    else {
+        cout << "Bike" << endl;
+    }
+return 0;
+}
